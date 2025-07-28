533de966201e1f414772ef24a9581d67df94b5ab
Debayan Dutta <debayan04dutta@gmail.com>
2025-07-28 15:29:08 +0000

    LeetCode: contains-duplicate

---
+++ leetcode/contains-duplicate/latest.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int a : nums){
+            if(s.count(a) != 0) return true;
+            s.insert(a);
+        }return false;
+    }
+};
