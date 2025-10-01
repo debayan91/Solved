34e56e45eb31ca3a67412174a685784c4c82a06e
Debayan Dutta <debayan04dutta@gmail.com>
2025-10-01 06:37:05 +0000

    LeetCode: water-bottles-ii

---
+++ leetcode/water-bottles-ii/latest.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int d = numBottles; //drunk 
+        int empty = numBottles; //initial empty
+        while(empty >= numExchange){
+            empty -= numExchange;
+            empty++;
+            d++;
+            numExchange++;
+        }
+        return d;
+    }
+};
