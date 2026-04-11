731f2df97990e4204781cbd366cbbf68195431e5
Debayan Dutta <debayan04dutta@gmail.com>
2026-04-11 17:30:32 +0000

    LeetCode: implement-stack-using-queues

---
+++ leetcode/implement-stack-using-queues/first.cpp
@@ -0,0 +1,33 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {}
+
+    void push(int x) { q.push(x); }
+
+    int pop() {
+        int size = q.size();
+        int n = size - 1;
+        for (int i = 0; i < n; i++) {
+            int temp = q.front();
+            q.pop();
+            q.push(temp);
+        }
+        int f = q.front();
+        q.pop();
+        return f;
+    }
+
+    int top() { return q.back(); }
+
+    bool empty() { return q.empty(); }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
---
+++ leetcode/implement-stack-using-queues/latest.cpp
@@ -0,0 +1,31 @@
+class MyStack {
+public:
+    queue<int> q;
+    MyStack() {}
+
+    void push(int x) { q.push(x); }
+
+    int pop() {
+        for (int i = 0; i < (q.size() - 1); i++) {
+            int temp = q.front();
+            q.pop();
+            q.push(temp);
+        }
+        int f = q.front();
+        q.pop();
+        return f;
+    }
+
+    int top() { return q.back(); }
+
+    bool empty() { return q.empty(); }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
