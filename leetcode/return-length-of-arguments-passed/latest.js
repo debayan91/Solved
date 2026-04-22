73cf4c1170d8590dadd6d65f947d0a27178145c4
Debayan Dutta <debayan04dutta@gmail.com>
2026-04-22 10:01:32 +0000

    LeetCode: return-length-of-arguments-passed

---
+++ leetcode/return-length-of-arguments-passed/latest.js
@@ -0,0 +1,13 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    var count = 0;
+    for(var args of args) count++;
+    return count;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
