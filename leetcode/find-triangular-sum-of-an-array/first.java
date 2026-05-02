e9c2de83546924b7dd2a69e68797bdba4f77f63d
Debayan Dutta <debayan04dutta@gmail.com>
2026-05-02 18:11:19 +0000

    LeetCode: find-triangular-sum-of-an-array

---
+++ leetcode/find-triangular-sum-of-an-array/first.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        int end = n - 1;
+        while (end != 0) {
+            for (int i = 0; i < end; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+            end--;
+        }
+        return nums[0];
+    }
+}
