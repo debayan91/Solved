ffae16a2b1caef05b78a1bc4c1555ef6540d7170
Debayan Dutta <debayan04dutta@gmail.com>
2026-05-16 10:38:51 +0000

    LeetCode: sort-the-students-by-their-kth-score

---
+++ leetcode/sort-the-students-by-their-kth-score/first.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& v, int k) {
+        sort(v.begin(), v.end(), [k](vector<int>& a, vector<int>& b) {
+            return a[k] > b[k];
+        });
+        return v;
+    }
+};
