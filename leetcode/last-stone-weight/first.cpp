5c6ba6947337c5874c513413a93a6d451a4a510c
Debayan Dutta <debayan04dutta@gmail.com>
2026-05-16 05:47:50 +0000

    LeetCode: last-stone-weight

---
+++ leetcode/last-stone-weight/first.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq;
+        for(int i : stones) pq.push(i);
+        while(pq.size() > 1){
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            pq.push(abs(x - y));
+        }
+        return pq.top();
+    }
+};
