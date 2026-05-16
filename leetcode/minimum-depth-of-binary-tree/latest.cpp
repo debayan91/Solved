cae48fe6f62ad03e20655efc0d2e9862f7f8b204
Debayan Dutta <debayan04dutta@gmail.com>
2026-05-16 11:47:22 +0000

    LeetCode: minimum-depth-of-binary-tree

---
+++ leetcode/minimum-depth-of-binary-tree/latest.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int min_depth = INT_MAX;
+    void res(TreeNode* root, int depth){
+        if(root->right == NULL && root->left == NULL){
+            min_depth = min(min_depth, depth);
+            return;
+        }
+        if(root->right != nullptr) res(root->right, depth + 1);
+        if(root->left != NULL) res(root->left, depth + 1);
+    }
+    int minDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        res(root, 1);
+        return min_depth;
+    }
+};
