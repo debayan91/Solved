bdc93b0a37bc39f615ef397d2fb6302c299cf94a
Debayan Dutta <debayan04dutta@gmail.com>
2026-05-23 15:52:49 +0000

    LeetCode: binary-search-tree-to-greater-sum-tree

---
+++ leetcode/binary-search-tree-to-greater-sum-tree/first.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    void inorder(TreeNode* root){
+        if(root == NULL) return;
+        inorder(root->right);
+
+        int value = root->val;
+        root->val += sum;
+        sum += value;
+        
+        inorder(root->left);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        inorder(root);
+        return root;
+    }
+};
