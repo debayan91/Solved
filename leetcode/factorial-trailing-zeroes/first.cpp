83364cdcb6eb82f86f08f992697de2a7e9c5fbe7
Debayan Dutta <debayan04dutta@gmail.com>
2026-06-04 10:00:02 +0000

    LeetCode: factorial-trailing-zeroes

---
+++ leetcode/factorial-trailing-zeroes/first.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        if(n < 5) return 0;
+        int count = 0;
+        for(int i = 5; i <= n; i *= 5){
+            count += n / i;
+        }
+        return count;
+    }
+};
