b290ac675f900e54b805c6951212c686f736acd4
Debayan Dutta <debayan04dutta@gmail.com>
2026-06-16 07:48:44 +0000

    LeetCode: process-string-with-special-operations-i

---
+++ leetcode/process-string-with-special-operations-i/first.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string a = "";
+        for (char& c : s) {
+            if (c == '*') {
+                if (size(a) > 0) a.pop_back();
+            } else if (c == '#') {
+                a = a + a;
+            } else if (c == '%') {
+                reverse(begin(a), end(a));
+            } else {
+                a = a + c;
+            }
+        }
+        return a;
+    }
+};
