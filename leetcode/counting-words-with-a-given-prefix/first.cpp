3d069886694f2561596b258ca84b60f717b841dc
Debayan Dutta <debayan04dutta@gmail.com>
2026-07-04 10:39:22 +0000

    LeetCode: counting-words-with-a-given-prefix

---
+++ leetcode/counting-words-with-a-given-prefix/first.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int count = 0;
+        for(string s : words) if(s.find(pref) == 0) count++;
+        return count;
+    }
+};
