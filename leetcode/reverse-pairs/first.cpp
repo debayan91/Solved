3b3c821b63a07866f85249da24ecfca5d1a2a4b8
Debayan Dutta <debayan04dutta@gmail.com>
2026-07-31 06:44:21 +0000

    LeetCode: reverse-pairs (First Accepted)

---
+++ leetcode/reverse-pairs/first.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int count = 0;
+    void merge(vector<int>& nums, int l, int mid, int r) {
+        vector<int> temp;
+        int i = l;
+        int j = mid + 1;
+
+        int p = i;
+        int q = mid + 1;
+
+        while(p <= mid && q <= r){
+            if(nums[p] > 2LL * nums[q]){
+                count += mid - p + 1;
+                q++;
+            } else p++;
+        }
+
+        while (i <= mid && j <= r) {
+            if (nums[i] <= nums[j]) {
+                temp.push_back(nums[i++]);
+            } else {
+                temp.push_back(nums[j++]);
+            }
+        }
+        while (i <= mid) {
+            temp.push_back(nums[i++]);
+        }
+        while (j <= r) {
+            temp.push_back(nums[j++]);
+        }
+        for (int k = l; k <= r; k++) {
+            nums[k] = temp[k - l];
+        }
+    }
+    void mergeSort(vector<int>& nums, int l, int r) {
+        if (l >= r)
+            return;
+        int mid = l + (r - l) / 2;
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid + 1, r);
+        merge(nums, l, mid, r);
+    }
+    int reversePairs(vector<int>& nums) {
+        mergeSort(nums, 0, size(nums) - 1);
+        return count;
+    }
+};
