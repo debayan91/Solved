3ecbb69159a7ce170801ef0857b22851e89599ba
Debayan Dutta <debayan04dutta@gmail.com>
2026-08-19 17:42:24 +0000

    LeetCode: count-number-of-nice-subarrays

---
+++ leetcode/count-number-of-nice-subarrays/first.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int goal) {
+        int l = 0, r = 0, n = size(nums), current = 0, res = 0;
+        if(goal == 0){
+            while(r < n){
+                if(nums[r] % 2 == 1) {r++; continue;}
+                int count = 0;
+                while(r < n && nums[r] == 0){
+                    count++;
+                    r++;
+                }
+                res += count * (count + 1) / 2;
+            }
+            return res;
+        }
+        while(r < n){
+            if(nums[r] % 2 == 1){
+                current++;
+            }
+            if(current == goal){
+                int a = 0, b = 0;
+                while(r + 1 < n && nums[r + 1] % 2 == 0){ // extend to the right
+                    r++;
+                    a++;
+                }
+                while(l < r && nums[l] % 2 == 0){ // extend to the left;
+                    l++;
+                    b++;
+                }
+                res += (a + 1) * (b + 1);
+                l++; // push out a 1
+                current--;
+            }
+            r++;
+        }
+        return res;
+    }
+};
